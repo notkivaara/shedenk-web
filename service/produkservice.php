59f89bf2d387d0eeed28e41352a3e8dc6810f0a2
SatriaYP <aremayuda@gmail.com>
2022-12-31 19:49:53 +0700

    rev produk service

--- service/produkservice.php
+++ service/produkservice.php
@@ -7,17 +7,19 @@ include '../database/koneksi.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $result = mysqli_query($kon, "SELECT * FROM produk");
-    $result2 = mysqli_query($kon, "SELECT produk.id, gambar.* FROM produk JOIN gambar ON produk.id = gambar.id_produk");
+    
     $list = array();
     $list_gambar = array();
     if (mysqli_num_rows($result) > 0) {
-        while ($row2 =mysqli_fetch_assoc($result2)){
-            array_push($list_gambar, array(
-                "id_produk" => $row2["id_produk"],
-                "gambar" => $row2["nama"],
-            ));
-        }
         while ($row = mysqli_fetch_assoc($result)) {
+            $id_gambar = $row['id'];
+            $result2 = mysqli_query($kon, "SELECT produk.id, gambar.* FROM produk JOIN gambar ON produk.id = gambar.id_produk WHERE id_produk = $id_gambar");
+            while ($row2 =mysqli_fetch_assoc($result2)){
+                array_push($list_gambar, array(
+                    "id_produk" => $row2["id_produk"],
+                    "gambar" => $row2["nama"],
+                ));
+            }
             array_push($list, array(
                 "id" => $row["id"],
                 "nama" => $row["nama"],
